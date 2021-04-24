3007ef771ac599cae299e3b39274b927e7d84d39
Abdulrahman <boodynaser@rocketmail.com>
2021-04-24 19:25:21 +0200

    Complete tutorial 0,1 and 2

--- zoo.sql
+++ zoo.sql
@@ -8,7 +8,67 @@ SELECT population FROM world
   SELECT name, area FROM world
   WHERE area BETWEEN 200000 AND 250000
 
-  /*tutorial 1 */
+  /*tutorial 1*/
+
+  SELECT name FROM world
+  WHERE name LIKE 'Y%'
+
+  SELECT name FROM world
+  WHERE name LIKE '%Y'
+
+  SELECT name FROM world
+  WHERE name LIKE '%x%'
+
+  SELECT name FROM world
+  WHERE name LIKE '%land'
+
+  SELECT name FROM world
+  WHERE name LIKE 'C%ia';
+
+  SELECT name FROM world
+  WHERE name LIKE '%oo%'
+
+  SELECT name FROM world
+  WHERE name LIKE '%a%%a%%a%';
+
+  SELECT name FROM world
+  WHERE name LIKE '_t%'
+  ORDER BY name;
+
+  SELECT name FROM world
+ WHERE name LIKE '%o__o%';
+
+  
+  SELECT name FROM world
+  WHERE name LIKE '____';
+
+  SELECT name
+  FROM world
+ WHERE name = capital ;
+
+ SELECT name
+  FROM world
+ WHERE capital = concat(name, ' City')
+
+ SELECT capital, name
+FROM world
+WHERE capital LIKE concat('%',name,'%');  
+
+SELECT capital, name
+FROM world
+WHERE capital LIKE concat(name,'_','%');
+
+SELECT name,  REPLACE( capital, name, '')AS ext
+FROM world
+WHERE capital LIKE concat(name,'_','%');
+
+  /*tutorial 2 */
+  SELECT name, continent, population FROM world;
+
+  SELECT name
+  FROM world
+ WHERE population >= 200000000;
+
   SELECT name,gdp/population FROM world WHERE population>200000000 
 
   SELECT name,population/1000000 from world WHERE continent='South America';
@@ -23,4 +83,19 @@ SELECT population FROM world
 
   SELECT name,ROUND(population/1000000,2) ,ROUND(GDP/1000000000,2) FROM world WHERE continent='South America';
 
-  
+  SELECT name,ROUND(GDP/population,-3) FROM world WHERE gdp>=1000000000000;
+
+  SELECT name,   
+       capital
+  FROM world
+ WHERE LENGTH(name)=LENGTH(capital);
+
+ SELECT name,capital
+FROM world WHERE LEFT(name,1)=LEFT(capital,1) AND /*LEFT return the nth letter */
+ name<>capital;
+
+SELECT name
+   FROM world
+WHERE name LIKE '%o%' and name LIKE '%a%' and name LIKE '%i%' and name LIKE '%u%' and name LIKE '%e%' 
+  AND name NOT LIKE '% %';
+
