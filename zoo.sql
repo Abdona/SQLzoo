bd6dc146a75739ba951856610309a805b75a0a83
Abdulrahman <boodynaser@rocketmail.com>
2021-04-30 08:20:07 +0200

    Complete tut 4

--- zoo.sql
+++ zoo.sql
@@ -99,3 +99,58 @@ SELECT name
 WHERE name LIKE '%o%' and name LIKE '%a%' and name LIKE '%i%' and name LIKE '%u%' and name LIKE '%e%' 
   AND name NOT LIKE '% %';
 
+/*Tutorial 4*/
+SELECT name FROM world
+  WHERE population >
+     (SELECT population FROM world
+      WHERE name='Russia')
+
+SELECT name FROM world
+  WHERE continent = 'Europe' AND
+  gdp/population >
+     (SELECT gdp/population FROM world
+      WHERE name='United Kingdom') 
+
+      SELECT name,continent FROM world
+WHERE continent =
+(SELECT continent FROM world WHERE name = 'Argentina')
+OR continent=
+(SELECT continent FROM world WHERE name = 'Australia') ORDER BY name
+
+SELECT name FROM world
+WHERE population >
+(SELECT population FROM world WHERE name = 'Canada')
+AND population<
+(SELECT population FROM world WHERE name = 'Poland') ORDER BY name
+
+SELECT name,CONCAT(ROUND(100*population/(SELECT population FROM world where name='Germany'),0),'%') AS percentage FROM world
+WHERE continent ='Europe'
+
+SELECT name
+  FROM world
+ WHERE gdp > ALL(SELECT gdp
+                           FROM world
+WHERE continent='Europe' AND gdp > 0
+                          )
+
+                          
+
+SELECT continent, name, area FROM world x
+  WHERE area >= ALL
+    (SELECT area FROM world y
+        WHERE y.continent=x.continent
+          AND population>0)
+
+
+SELECT continent,name FROM world x
+WHERE name<=ALL(SELECT name FROM world y WHERE x.continent=y.continent
+ ORDER BY name
+) ORDER BY continent
+
+SELECT name,continent,population FROM world x
+where name = ALL(SELECT continent FROM world y
+WHERE x.continent=y.continent AND y.population >=25000000 )
+
+SELECT name,continent FROM world x
+WHERE population >= ALL(SELECT 3*population FROM world y
+WHERE x.continent = y.continent  AND x.name <> y.name)
